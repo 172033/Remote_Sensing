195a072b45c131d9eb645757b05072de80144dd4
172033 <s172033@student.dtu.dk>
2017-12-01 00:58:37 +0100

    order of brightness temperatures fixed

--- SimpleTemporaryHandleForInverseModel.m
+++ SimpleTemporaryHandleForInverseModel.m
@@ -2,16 +2,16 @@
 % (6.93v, 6.93h, 10.65v, 10.65h, 18.70v, 18.70h, 23.80v, 23.80h, 36.50v, 36.50h)
 
 nor_SIC0 = 6988;
-nor_SIC1 = 15965;
+%nor_SIC1 = 15965;
 
 SIC0 = csvread('SIC0_noHeader.csv', 1, 25, [1 25 nor_SIC0 37]);
-SIC1 = csvread('SIC1_noHeader.csv', 1, 25, [1 25 nor_SIC1 37]);
+%SIC1 = csvread('SIC1_noHeader.csv', 1, 25, [1 25 nor_SIC1 37]);
 
 % SIC0
-TB = [SIC0(:,1) SIC0(:,2) SIC0(:,5) SIC0(:,6) SIC0(:,7) SIC0(:,8) SIC0(:,9) SIC0(:,10) SIC0(:,11) SIC0(:,12)];
+TB = [SIC0(:,2) SIC0(:,1) SIC0(:,6) SIC0(:,5) SIC0(:,8) SIC0(:,7) SIC0(:,10) SIC0(:,9) SIC0(:,12) SIC0(:,11)];
 
 %SIC1
-%TB = [SIC1(:,1) SIC1(:,2) SIC1(:,5) SIC1(:,6) SIC1(:,7) SIC1(:,8) SIC1(:,9) SIC1(:,10) SIC1(:,11) SIC1(:,12)];
+%TB = [SIC0(:,2) SIC0(:,1) SIC0(:,6) SIC0(:,5) SIC0(:,8) SIC0(:,7) SIC0(:,10) SIC0(:,9) SIC0(:,12) SIC0(:,11)];
 
 % Calculating p from SIC0 using inverse model
 p_SIC0 = zeros(nor_SIC0,7);
